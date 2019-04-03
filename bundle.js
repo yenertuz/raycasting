7c86954a9e9bacace75f01c441910b9670d9fa84
yenertuz <yenertuz@hotmail.com>
2019-04-03 14:12:09 -0700

    rebuild main

--- bundle.js
+++ bundle.js
@@ -1,19 +1,33 @@
-function start_game(state) {
-  console.log("game started");
+function get_state_for_main() {
+  let state = {};
+  
+  state.canvas = document.getElementById("root");
+  state.context = state.canvas.getContext("2d");
+  state.canvas.width = state.canvas.clientWidth;
+  state.canvas.height = state.canvas.clientHeight;
+  return (state);
 }
 
-function display_intro(state) {
+function clear_canvas(state) {
+  state.context.fillStyle = "white";
+  state.context.fillRect(0, 0, state.canvas.width, state.canvas.height);
+}
+
+function handle_resize(state) {
+  state.canvas.width = state.canvas.clientWidth;
+  state.canvas.height = state.canvas.clientHeight;
+  clear_canvas(state);
   state.context.font = "30px Arial";
-  state.context.fillText("Hello World", 10, 50);
+  state.context.fillStyle = "black";
+  state.context.fillText("Resize handled", 10, 50);
 }
 
 function main() {
-  var state = {};
-  state.canvas = document.getElementById("root");
-  state.context = state.canvas.getContext("2d");
-  display_intro(state);
-  start_game(state);
+  var state = get_state_for_main();
+
+  window.addEventListener("resize", () => { handle_resize(state); });
+  state.context.font = "30px Arial";
+  state.context.fillText("Initial display", 10, 50);
 }
 
-console.log("about to start");
 main();
