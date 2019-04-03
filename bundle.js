813c84f86e3ed07b88513bfbfdd5fecff71bf583
Yener Tuz <32311607+yenertuz@users.noreply.github.com>
2019-04-03 10:01:01 -0700

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,4 +1,17 @@
-var c = document.getElementById("root");
-var ctx = c.getContext("2d");
-ctx.font = "30px Arial";
-ctx.fillText("Hello World", 10, 50);
+function start_game() {
+  console.log("game started");
+}
+
+function display_intro() {
+  context.font = "30px Arial";
+  context.fillText("Hello World", 10, 50);
+}
+
+function main() {
+  var canvas = document.getElementById("root");
+  var context = canvas.getContext("2d");
+  display_intro();
+  start_game();
+}
+
+main();
